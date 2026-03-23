905c4073786f1f1aa1a6f414d52310fb704ad5de
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-03-23 11:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create student object
+const student = {
+  name: "John"
+};
+
+// Function to get all keys
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Example test
+console.log(getKeys(student)); // ["name"]
