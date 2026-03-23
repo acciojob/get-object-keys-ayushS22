4795103858ce5cb7fc4f59c2b2284ae3997032c3
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-03-23 11:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-// Create object with function inside it
 const myObj = {
   getKeys: function(obj) {
     return Object.keys(obj);
   }
 };
-
-// Example usage
-const student = {
-  name: "John",
-  age: 20,
-  city: "Delhi"
-};
-
-console.log(myObj.getKeys(student)); 
-// ["name", "age", "city"]
